b04ff6449891852644debab7a9c39423127ce453
kobnuhok <77298941+kobnuhok@users.noreply.github.com>
2024-05-07 16:27:32 +0300

    Добавил фронт часть в папку public

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
-// Здесь функциональность точки входа
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
+const path = require('path');
 
 const PORT = 3000;
 const app = express();
 
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/",(req, res) => {
   fs.readFile("./public/index.html", "utf-8").then (data => {
     res.header("Content-Type", "text/html");
     res.send(data);
   })
-  res.send("Hello World!");
 })
-// // Здесь данные
-// const games = [
-//   { 
-//     id: 1,
-//       title: "Crystal Keeper",
-//       description: "Управляй боевым дроном, чтобы любой ценой защитить кристалл от враждебных космо-слизней."
-//   },
-//   { 
-//     id: 2,
-//     title: "Dangeons'n'Caves. Prologue", 
-//     description: "Безымянный герой исследует пещеры и подземелья, чтобы найти больше информации о себе."
-//   },
-// ];
 
-// // Здесь роутинг
-// app.get('/games/:id', (req, res) => {
-//   if (!games[req.params.id]) {
-//     res.send(`Такой игры не существует`);
 
-//     // Не забудем выйти из функции
-//     return;
-//   }
 
-//   const { title, description } = users[req.params.id];
-  
-//   res.send(`Новое издание игры - ${name}, откроет для вас новый мир: ${description}`);
-// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at PORT http://localhost:${PORT}`);
-});
+
+
+
+
+
+
+
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,11 @@
-// routes/main.js
-const mainRoute = require("express").Router();
 
-mainRoute.get("/", (req, res) => {
-  res.status(200).send('<h1>Скоро тут будет интерфейс</h1>');
-});
-
-module.exports = mainRoute;
+     const mainRoute = require("express").Router();
+     
+     mainRoute.get("/", (req, res) => {
+       fs.readFile("./public/index.html", "utf-8").then((data) => {
+         res.header("Content-Type", "text/html").send(data);
+       });
+     });
+     
+     module.exports = mainRoute;
+     
