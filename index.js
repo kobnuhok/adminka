c7efba042a0d2fd2a3a029ed925fa8c7e5e9c0c4
kobnuhok <77298941+kobnuhok@users.noreply.github.com>
2024-05-09 11:17:47 +0300

    Дэзэшечка на проверку

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mainRoute = require('./routes/main');
 const gamesRouter = require('./routes/games');
 
+const cors = require('./middlewares/cors');
 
 const path = require('path');
 
@@ -11,6 +12,7 @@ const PORT = 3000;
 const app = express();
 
 app.use(
+  cors,
   bodyParser.json(),
   express.static(path.join(__dirname, 'public')),
   mainRoute, 
